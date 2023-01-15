222e48cf50f263a9646556020018a35e16aef78d
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-15 17:51:24 +0900

    Declaration Files

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "TypeScript";
-  }
-}
+import { init, exit } from "myPackage";
+
+init({
+  url: "true",
+});
+
+exit(1);
