93da142f91dd37f81dfdb681ee1e3cf18ac61ab5
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-01-15 18:29:00 +0900

    DefinitelyTyped

--- src/index.ts
+++ src/index.ts
@@ -8,10 +8,13 @@ interface BlockShape {
 }
 class Block implements BlockShape {
   public hash: string;
+  // 블록생성 하고 데이터를 받음
   constructor(public prevHash: string, public height: number, public data: string) {
     this.hash = Block.calculateHash(prevHash, height, data);
   }
   static calculateHash(prevHash: string, height: number, data: string) {
+    // 그 데이터의 해쉬값은 해당 코드에서 생성하게 됨
     const toHash = `${prevHash}${height}${data}`;
+    return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
